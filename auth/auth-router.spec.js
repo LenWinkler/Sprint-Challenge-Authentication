64f12285e8c002fd845d51bc41b7ab70e06f4b83
LenWinkler <lenwinkler86@gmail.com>
2019-11-15 13:57:58 -0500

    added tests for login endpoint

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const assert = require('assert');
 
 const server = require('../api/server.js');
 
@@ -28,4 +29,32 @@ describe('auth endpoints', function() {
             .expect('Content-Type', /json/i)
         })
     })
-})
+
+    describe('POST /api/auth/login', function() {
+
+        it('should return 200 OK', async function() {
+
+            return await request(server)
+            .post('/api/auth/login')
+            .send({ username: "Len", password: "password" })
+            .set('Accept', 'application/json')
+            .expect(200)
+        })
+
+        it('should return a token', async function() {
+
+            return await request(server)
+            .post('/api/auth/login')
+            .send({ username: "Len", password: "password" })
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(res => {
+                assert(res.body.token)
+            })
+            })
+        })
+    })
+
+    const hasToken = res => {
+        res.body.should.have.property("token");
+    }
