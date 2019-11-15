ff6164bd1097d64ff39ffcc30a5062e73c831fb5
LenWinkler <lenwinkler86@gmail.com>
2019-11-15 11:45:10 -0500

    added users-model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
 });
 
 router.post('/login', (req, res) => {
